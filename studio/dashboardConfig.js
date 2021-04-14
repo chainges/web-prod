b882c2a1a24eb733ce90eb9cfb1311f8d74ed148
Sanity.io <hello@sanity.io>
2021-04-14 20:54:44 +0200

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,47 @@
+export default {
+  widgets: [
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '60773a71d4c2651e752e6e01',
+                  title: 'Sanity Studio',
+                  name: 'sanity-kitchen-sink-studio-zvkupi8f',
+                  apiId: 'e8b93f4a-0151-4e21-80a1-ef8e2e989f24'
+                },
+                {
+                  buildHookId: '60773a711cb1c51dae7a424a',
+                  title: 'Blog Website',
+                  name: 'sanity-kitchen-sink-web-p54onu7p',
+                  apiId: 'ca9922f3-ba8a-4531-8af1-cf3e2a194e85'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/emelleby/sanity-kitchen-sink',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: 'https://sanity-kitchen-sink-web-p54onu7p.netlify.app', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
