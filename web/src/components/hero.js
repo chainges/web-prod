acce24367d799f961d5b57ed23ef52a6a2938d43
SavyasachiInfotech <dev.savyasachiinfotech@gmail.com>
2021-04-19 19:34:09 +0530

    Change in hero.js, infoRows.js, footer.js

--- web/src/components/hero.js
+++ web/src/components/hero.js
@@ -1,36 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PortableText from "./portableText";
 import clientConfig from "../../client-config";
 import CTALink from "./CTALink";
 
 import { getGatsbyImageData } from "gatsby-source-sanity";
 import { GatsbyImage } from "gatsby-plugin-image";
-const maybeImage = illustration => {
-  let img = null;
-  if (illustration && illustration.image && illustration.image.asset && !illustration.disabled) {
-    const imageData = getGatsbyImageData(
-      illustration.image,
-      { maxWidth: 960 },
-      clientConfig.sanity
-    );
-
-    img = (
-      <GatsbyImage
-        className="w-full md:w-4/5 z-50"
-        image={imageData}
-        alt={illustration.image.alt}
-      />
-    );
-  }
-  return img;
-};
 
 function Hero(props) {
-  const img = maybeImage(props.illustration);
+  const [imageData, setImageData] = useState(null);
+  useEffect(() => {
+    if (props.illustration && props.illustration.image && props.illustration.image.asset && !props.illustration.disabled) {
+      setImageData(getGatsbyImageData(
+        props.illustration.image,
+        { maxWidth: 960 },
+        clientConfig.sanity
+      ));
+    }
+  }, [props.illustration])
+  // setImg(maybeImage(props.illustration));
   return (
     <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       {/* Left col */}
-      <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+      <div className="flex flex-col w-full md:w-3/6 justify-center items-start text-center md:text-left">
         <p className="uppercase tracking-loose w-full">{props.label}</p>
         <h1 className="my-4 text-5xl font-bold leading-tight">{props.heading}</h1>
         <div className="leading-normal text-2xl mb-8">
@@ -44,7 +35,16 @@ function Hero(props) {
         )}
       </div>
       {/* Right col */}
-      <div className="w-full md:w-3/5 py-6 text-center">{img}</div>
+      <div className="w-full md:w-3/6 py-6 ml-auto text-center">
+        {imageData ? (
+          <GatsbyImage
+            className="w-full md:w-4/5 "
+            image={imageData}
+            alt={props.illustration.image.alt}
+          />
+        ) : (
+          <></>
+        )}</div>
     </div>
   );
 }
