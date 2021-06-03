ef899c44495ac09d929ba2064638f9035a036f24
harshil-270 <harshiltagadiya@gmail.com>
2021-06-03 12:25:55 +0530

    footer localization

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -1,107 +1,110 @@
 import { Link } from "gatsby";
+import { FormattedMessage } from "gatsby-plugin-intl";
 import React from "react";
 
-const Footer = ({ siteTitle }) => (
-  <footer className="bg-white">
-    <div className="container mx-auto  px-8">
-      <div className="w-full flex flex-col md:flex-row py-6">
-        <div className="flex-1 mb-6 mr-8">
-          <a
-            className="text-orange-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-            href="#"
-          >
-            {siteTitle}
-          </a>
-        </div>
+const Footer = ({ siteTitle }) => {
+  return (
+    <footer className="bg-white">
+      <div className="container mx-auto  px-8">
+        <div className="w-full flex flex-col md:flex-row py-6">
+          <div className="flex-1 mb-6 mr-8">
+            <a
+              className="text-orange-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+              href="#"
+            >
+              <FormattedMessage id="siteTitle" />
+            </a>
+          </div>
 
-        <div className="flex-1">
-          <p className="uppercase text-gray-500 md:mb-6">Links</p>
-          <ul className="list-reset mb-6">
-            <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a href="https://www.tailwindtoolbox.com/templates/landing-page">
-                <span className="hover:underline text-gray-800 hover:text-orange-500">
-                  Page theme
+          <div className="flex-1">
+            <p className="uppercase text-gray-500 md:mb-6"><FormattedMessage id="links.title" /></p>
+            <ul className="list-reset mb-6">
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a href="https://www.tailwindtoolbox.com/templates/landing-page">
+                  <span className="hover:underline text-gray-800 hover:text-orange-500">
+                  <FormattedMessage id="links.pageTheme" />
+                  </span>
+                </a>
+              </li>
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="https://sanity.io/docs"
+                  className="no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >
+                  <FormattedMessage id="links.sanityHelp" />
+                </a>
+              </li>
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="https://slack.sanity.io"
+                  className="no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >
+                  <FormattedMessage id="links.slackCommunity" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="flex-1">
+            <p className="uppercase text-gray-500 md:mb-6"><FormattedMessage id="legal.title" /></p>
+            <ul className="list-reset mb-6">
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <span className="no-underline hover:underline text-gray-800 hover:text-orange-500">
+                <FormattedMessage id="legal.terms" />
                 </span>
-              </a>
-            </li>
-            <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="https://sanity.io/docs"
-                className="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >
-                Sanity Help
-              </a>
-            </li>
-            <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="https://slack.sanity.io"
-                className="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >
-                Slack Community
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="flex-1">
-          <p className="uppercase text-gray-500 md:mb-6">Legal</p>
-          <ul className="list-reset mb-6">
-            <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-              <span className="no-underline hover:underline text-gray-800 hover:text-orange-500">
-                Terms
-              </span>
-            </li>
-            <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-              <span className="no-underline hover:underline text-gray-800 hover:text-orange-500">
-                Privacy
-              </span>
-            </li>
-          </ul>
-        </div>
-        <div className="flex-1">
-          <p className="uppercase text-gray-500 md:mb-6">Social</p>
-          <ul className="list-reset mb-6">
-            <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="https://twitter.com/sanity_io"
-                className="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >
-                Twitter
-              </a>
-            </li>
-            <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="https://www.linkedin.com/company/sanity-io/"
-                className="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >
-                Linkedin
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="flex-1">
-          <p className="uppercase text-gray-500 md:mb-6">Company</p>
-          <ul className="list-reset mb-6">
-            <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-              <Link
-                to="/blog"
-                className="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >
-                Blog
-              </Link>
-            </li>
-            <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="https://www.sanity.io/contact"
-                className="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
+              </li>
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <span className="no-underline hover:underline text-gray-800 hover:text-orange-500">
+                <FormattedMessage id="legal.privacy" />
+                </span>
+              </li>
+            </ul>
+          </div>
+          <div className="flex-1">
+            <p className="uppercase text-gray-500 md:mb-6"><FormattedMessage id="social.title" /></p>
+            <ul className="list-reset mb-6">
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="https://twitter.com/sanity_io"
+                  className="no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >
+                  <FormattedMessage id="social.twitter" />
+                </a>
+              </li>
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="https://www.linkedin.com/company/sanity-io/"
+                  className="no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >
+                  <FormattedMessage id="social.linkedin" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="flex-1">
+            <p className="uppercase text-gray-500 md:mb-6"><FormattedMessage id="company.title" /></p>
+            <ul className="list-reset mb-6">
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <Link
+                  to="/blog"
+                  className="no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >
+                  <FormattedMessage id="company.blog" />
+                </Link>
+              </li>
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="https://www.sanity.io/contact"
+                  className="no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >
+                  <FormattedMessage id="company.contact" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  </footer>
-);
+    </footer>
+  )
+};
 
 export default Footer;
