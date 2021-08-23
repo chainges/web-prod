b4d4a23bae6e0d6aa3bc6ad7fcd6cabf7716e436
harshilredorange <harshil@redorangetechnologies.com>
2021-08-23 23:07:13 +0530

    footer redesign

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -6,56 +6,27 @@ import React from "react";
 const Footer = ({ siteTitle }) => {
   return (
     <footer className="bg-white">
-      <div className="container mx-auto  px-8">
-        <div className="w-full flex flex-col md:flex-row py-6">
+      <div className="container mx-auto px-8">
+        <div className="w-full flex flex-col sm:flex-row py-6">
           <div className="flex-1 mb-6 mr-8">
             <a
-              className="text-orange-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+              className="text-orange-600 no-underline hover:no-underline font-bold text-2xl md:text-4xl"
               href="#"
             >
               <FormattedMessage id="siteTitle" />
             </a>
-          </div>
-
-          <div className="flex-1">
-            <p className="uppercase text-gray-500 md:mb-6"><FormattedMessage id="links.title" /></p>
-            <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="https://www.tailwindtoolbox.com/templates/landing-page">
-                  <span className="hover:underline text-gray-800 hover:text-orange-500">
-                  <FormattedMessage id="links.pageTheme" />
-                  </span>
-                </a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="https://sanity.io/docs"
-                  className="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  <FormattedMessage id="links.sanityHelp" />
-                </a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="https://slack.sanity.io"
-                  className="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  <FormattedMessage id="links.slackCommunity" />
-                </a>
-              </li>
-            </ul>
           </div>
           <div className="flex-1">
             <p className="uppercase text-gray-500 md:mb-6"><FormattedMessage id="legal.title" /></p>
             <ul className="list-reset mb-6">
               <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                 <span className="no-underline hover:underline text-gray-800 hover:text-orange-500">
-                <FormattedMessage id="legal.terms" />
+                <FormattedMessage id="legal.termsOfService" />
                 </span>
               </li>
               <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                 <span className="no-underline hover:underline text-gray-800 hover:text-orange-500">
-                <FormattedMessage id="legal.privacy" />
+                <FormattedMessage id="legal.privacyAndCookies" />
                 </span>
               </li>
             </ul>
@@ -65,18 +36,18 @@ const Footer = ({ siteTitle }) => {
             <ul className="list-reset mb-6">
               <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                 <a
-                  href="https://twitter.com/sanity_io"
+                  href=" https://www.linkedin.com/company/scope321"
                   className="no-underline hover:underline text-gray-800 hover:text-orange-500"
                 >
-                  <FormattedMessage id="social.twitter" />
+                  <FormattedMessage id="social.linkedin" />
                 </a>
               </li>
               <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                 <a
-                  href="https://www.linkedin.com/company/sanity-io/"
+                  href="https://twitter.com/scope321as"
                   className="no-underline hover:underline text-gray-800 hover:text-orange-500"
                 >
-                  <FormattedMessage id="social.linkedin" />
+                  <FormattedMessage id="social.twitter" />
                 </a>
               </li>
             </ul>
@@ -86,19 +57,19 @@ const Footer = ({ siteTitle }) => {
             <ul className="list-reset mb-6">
               <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                 <Link
-                  to="/blog"
+                  to="/about"
                   className="no-underline hover:underline text-gray-800 hover:text-orange-500"
                 >
-                  <FormattedMessage id="company.blog" />
+                  <FormattedMessage id="company.OmScope321" />
                 </Link>
               </li>
               <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="https://www.sanity.io/contact"
+                <Link
+                  to="/contact"
                   className="no-underline hover:underline text-gray-800 hover:text-orange-500"
                 >
                   <FormattedMessage id="company.contact" />
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
